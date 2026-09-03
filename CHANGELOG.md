c5aebe2ca8841e155bbfc05e6610c5cf3f8c52f5
Jayden <linjie@fireworkhq.com>
2026-09-03 03:52:39 +0000

    Update sample codes for v2.21.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # CHANGELOG
 
+## [2.21.5]
+
+### Fixed
+
+- Bug fixes and improvements
+
 ## [2.21.4]
 
 ### Fixed
--- FireworkRNSDKSample/src/screens/More.tsx
+++ FireworkRNSDKSample/src/screens/More.tsx
@@ -24,7 +24,7 @@ import FireworkSDK, {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import StorageKey from '../constants/StorageKey';
 
-const fwNativeVersionOfAndroid = '6.34.4';
+const fwNativeVersionOfAndroid = '6.34.6';
 
 type MoreScreenNavigationProp = CompositeNavigationProp<
   BottomTabNavigationProp<TabParamsList, 'More'>,
